ffe04146f740cfe6e5ea542cda8fcd1bca279dfd
Muhammad Salman Abid <ealahherp@gmail.com>
2019-08-05 21:51:47 +0700

    bcrypt pass

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
              'email'    => $request->email,
-             'password' => $request->password,
+             'password' =>  bcrypt($request->password),
          ]);
 
         $token = auth()->login($user);
